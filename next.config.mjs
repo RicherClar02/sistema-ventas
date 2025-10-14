89a47fff927e990bcad6529f1c759cdc944c77e2
go02 <brayanclarosdiaz@gmail.com>
2025-10-14 17:29:02 -0500

    Ready for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ]
+  },
+   images: {
+    domains: [
+      'images.unsplash.com',
+      'lh3.googleusercontent.com',
+      'tu-dominio-de-imagenes.com'
+    ],
+  },
+  experimental: {
+    appDir: true,
+  },
+  // Para PWA
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+}
